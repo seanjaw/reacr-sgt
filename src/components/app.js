cf75be8756bdd5ef4d8c6d1749cb6a39309dfc16
seanjaw <sjaw94@gmail.com>
2019-02-08 15:44:54 -0800

    completed displaying student data from dummy data

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css'
 import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import React from 'react';
+import Table from './table';
+
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
-        </div>
+        <h1 className='center'>SGT</h1>
+        <Table/>
     </div>
 );
 
---
+++ src/data/get_all_students.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name:'Steve B.',
+        course: 'Math 101',
+        grade: 76
+    },
+    {
+        id: 2,
+        name: 'Karen C.',
+        course:'English 93',
+        grade: 98
+    },
+    {
+        id: 3, 
+        name: 'Brett A.',
+        course: 'English 401',
+        grade: 99
+    },
+    {
+        id: 4,
+        name: 'Cody M',
+        course: 'Intro to Legos',
+        grade: 100
+    }
+    ];
